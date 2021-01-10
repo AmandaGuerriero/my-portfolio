a2b7c45e793ccec466d4a7feec2424cc5c59b06f
Amanda Guerriero <ag.codingclass@gmail.com>
2021-01-09 16:57:40 -0800

    styling of gird items

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,9 +5,9 @@ function Resume(props) {
     return (
         <section class="page-section">
             <h2>Resume</h2>
-            <div>
+            <div class="resume">
                 <a href="https://github.com/amandaguerriero/my-portfolio/raw/master/src/images/Resume.pdf"><img src="https://github.com/amandaguerriero/my-portfolio/raw/master/src/images/Resume.png"></img></a>
-                <h3>Click image above to download resume</h3>
+                <h3><br />Click image above to download resume</h3>
             </div>
         </section>
     );
